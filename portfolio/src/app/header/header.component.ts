0384f80fb7f6de45142171beaee79606a9a08030
ctgschollar <cschollar@ska.ac.za>
2022-09-29 09:37:01 +0200

    Header style/structure complete with typewriter
    Created the website header with a typewriter effect for the title.
    
    I allowed printing to console with .tslint so I can see my logging.
    
    I tried to update a property using the KeyOf function, but it failed.
    I can access the property, but I cannot update its value.
    Not sure why... I need to move on, but an interesting problem to
    figure out in the future.

---
+++ portfolio/src/app/header/header.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css', '../app.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title: string;
+
+  constructor() {
+    this.title="test";
+  }
+
+  ngOnInit(): void {
+    this.typewriter("title","schollar.dev", 3);
+  }
+
+  sleep(ms: number): Promise<boolean> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async typewriter(property: keyof HeaderComponent, text:string, period:number): Promise<void> {
+    let sleepPeriod: number = period / text.length * 1000;
+    for (let i = 0; i < 4; i++) {
+      this["title"] = i % 2 === 0 ? "|" : "";
+      await this.sleep(sleepPeriod);
+    }
+    for (let i = 0; i < text.length +1; i++){
+      // this[property] = text;
+      this["title"] = text.substring(0,i) + (i % 2 === 0 ? "|" : "") ;
+      await this.sleep(sleepPeriod);
+    }
+    for (let i = 1; i < 6; i++) {
+      this["title"] =  text + (i % 2 === 0 ? "|" : "");
+      await this.sleep(sleepPeriod);
+    }
+    this["title"] = text
+  }
+}
